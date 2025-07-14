a180b0298daf3d2191b7bd56a2e81082d7992811
Specter2100 <bitcoinspecter@gmail.com>
2025-07-14 16:47:28 +0900

    Fix the bug where the recent block is processed twice.

--- chaintipval.go
+++ chaintipval.go
@@ -77,7 +77,7 @@ func main() {
 
 	// 데이터베이스 경로 설정
 	//Fast Filtered Level Data Base
-	dbPath := filepath.Join(dataDir, "blocks_ffldb")
+	dbPath := filepath.Join(dataDir, "Blocks_ffldb")
 
 	// 데이터베이스 없으면 생성
 	if _, err := os.Stat(dbPath); os.IsNotExist(err) {
@@ -165,9 +165,9 @@ func connectToNode(nodeIP string, netParams *chaincfg.Params, chain *blockchain.
 // 핸드쉐이크 완료 후 블록 요청 과정으로 블록 요청 하나하나 보는
 // requestBlocks: 전체 흐름 관리
 func requestBlocks(conn net.Conn, netParams *chaincfg.Params, chain *blockchain.BlockChain) error {
-	targetBlockHash, err := chainhash.NewHashFromStr("0000001039d4ae508fede893a00d524b03a92af2a7142680cff2bbd1113b35a3")
+	targetBlockHash, err := chainhash.NewHashFromStr("000000070bc517a758501190df354a9504a96fd86d18df0ee4c4ae26bbfb265d")
 	if err != nil {
-		return fmt.Errorf("invalid target block hash: %v", err)
+		return fmt.Errorf("Invalid target block hash: %v", err)
 	}
 
 	locator, err := chain.LatestBlockLocator()
@@ -194,39 +194,34 @@ func sendGetBlocks(conn net.Conn, netParams *chaincfg.Params, blockLocator []*ch
 	}
 	err := wire.WriteMessage(conn, getBlocksMsg, wire.FeeFilterVersion, netParams.Net)
 	if err != nil {
-		return fmt.Errorf("failed to send getblocks message: %v", err)
+		return fmt.Errorf("Failed to send getblocks message: %v", err)
 	}
 	return nil
 }
 
 // processMessages: 메시지 수신 및 처리 루프/ 메시지 수신 루프를 관리. 각 메시지 타입에 맞는 핸들러 함수 호출.
-// 여기서 걸리니 에러가 나오는거 아닌가 얘는 왜 processmessages를 go to def하면 중복되게 나오지??
 func processMessages(conn net.Conn, netParams *chaincfg.Params, chain *blockchain.BlockChain, targetBlockHash *chainhash.Hash) error {
 	blocksInQueue := make(map[chainhash.Hash]struct{}) //요청 중인 블록 해시를 추적하는거
-	fmt.Println("여기니")
 
 	for {
-		_, msg, bytes, err := wire.ReadMessageWithEncodingN(conn, wire.FeeFilterVersion, netParams.Net, wire.WitnessEncoding) //
-		fmt.Println("여기냐")
+		_, msg, bytes, err := wire.ReadMessageWithEncodingN(conn, wire.FeeFilterVersion, netParams.Net, wire.WitnessEncoding)
+		// 메시지 읽음 연결,버전,네트워크종류,세그윗지원하는, 바이트는 메세지 바트
 		if err != nil {
 			log.Printf("Failed to read message: %v, %v", err, msg) // 에러 발생시 로그 남기고 진행
 			continue
 		}
 
 		switch m := msg.(type) {
-		case *wire.MsgInv: // MsgInv 처리/ 블록 해시를 포함하는 inv 메시지를 처리하고 getdata 요청
-			err = handleInvMessage(m, chain, conn, netParams, blocksInQueue) // inv 메시지 처리 함수 호출
-			fmt.Println("에러")
+		case *wire.MsgInv: // MsgInv 처리/ 블록 해시를 포함하는 inv 메시지를 처리하고 getdata 요청 inventory는 Inventory vectors are used for notifying other nodes about objects they have or data which is being requested.
+			err = handleInvMessage(m, chain, conn, netParams, blocksInQueue) // inv 메시지 처리 함수 호출                                          //500개 큐 받을때 뜸 에러,여기냐,에러2 순서로
 			if err != nil {
-				fmt.Println("여기 에러")
 				return err
 			}
 
-		case *wire.MsgBlock: // MsgBlock 처리/ 블록 메시지 처리하고 블록에 검증, 체인 업뎃, 목표 블록도 확인
+		case *wire.MsgBlock: // MsgBlock 처리 / 블록 메시지 처리하고 블록에 검증, 체인 업뎃, 목표 블록도 확인
+			//MsgBlock은 Message 인터페이스를 구현하며 비트코인 블록 메시지를 나타냄, 주어진 블록 해시에 대한 getdata 메시지(MsgGetData)에 응답하여 블록 및 트랜잭션 정보를 전달하는 데 사용
 			block, err := btcutil.NewBlockFromBytes(bytes)
-			fmt.Println("에러2")
 			if err != nil {
-				fmt.Println("여기 에러2")
 				return err
 			}
 			err = handleBlockMessage(block, chain, blocksInQueue, targetBlockHash, conn, netParams) // 블록 메세지 처리 함수 호출
@@ -253,12 +248,13 @@ func processMessages(conn net.Conn, netParams *chaincfg.Params, chain *blockchai
 
 // handleInvMessage: MsgInv 처리/ getdata 요청을 보내고, 빈 InvList일 때 추가 getblocks 요청
 func handleInvMessage(m *wire.MsgInv, chain *blockchain.BlockChain, conn net.Conn, netParams *chaincfg.Params, blocksInQueue map[chainhash.Hash]struct{}) error {
-	getDataMsg := wire.NewMsgGetData() // getdata message 생성
-	for _, inv := range m.InvList {    // inv 리스트 for룹
+	getDataMsg := wire.NewMsgGetData()
+	fmt.Println("creat getdata message") // getdata message 생성
+	for _, inv := range m.InvList {      // inv 리스트 for룹
+		//fmt.Println(inv.Hash.String())
 		if inv.Type == wire.InvTypeBlock { // 만약 블록 타입이면
-			fmt.Println("on item", inv.Hash.String()) // 현재 아이템 해시 출력하고
+			fmt.Println("On Block Hash", inv.Hash.String()) // 현재 블록 해시 출력하고
 			if chain.IsKnownOrphan(&inv.Hash) {
-				fmt.Println("261error") // 체인에 알려진 고아블록인지 확인하고 계속 해라
 				continue
 			}
 			inv.Type = wire.InvTypeWitnessUtreexoBlock //지정하는것 InvWitnessBlock, InvTypeWitnessUtreexoBlock , 얘를 처음쓸지말지는 프로토콜 이해도에따라 다름, 그 다음 어떤거를 쓸지는 점프데프니션으로 알 수 있다.
@@ -269,28 +265,36 @@ func handleInvMessage(m *wire.MsgInv, chain *blockchain.BlockChain, conn net.Con
 
 	err := wire.WriteMessage(conn, getDataMsg, wire.FeeFilterVersion, netParams.Net) // getdata 메세지 전송
 	if err != nil {                                                                  // 에러면 리턴 반환
-		return fmt.Errorf("failed to send getdata message: %v", err)
+		return fmt.Errorf("Failed to send getdata message: %v", err)
 	}
 	return nil // 에러 없으면 nil 반환
 }
 
 // handleBlockMessage: MsgBlock 처리/블록 검증, 체인 추가, 목표 블록 확인, 추가 요청 로직 포함, 수신된 블록 메시지를 처리하여 체인에 추가하고, 동기화 상태를 관리하며, 타겟 블록에 도달했는지 확인
 func handleBlockMessage(block *btcutil.Block, chain *blockchain.BlockChain, blocksInQueue map[chainhash.Hash]struct{}, targetBlockHash *chainhash.Hash, conn net.Conn, netParams *chaincfg.Params) error {
-	delete(blocksInQueue, *block.Hash())
+	delete(blocksInQueue, *block.Hash()) // 현재 블록 해시를 대기 중인 블록 맵에서 제거
 	snapshot := chain.BestSnapshot()
-	fmt.Printf("best height %v, hash %v, got block %v\n", snapshot.Height, snapshot.Hash, block.Hash())
+	fmt.Printf("Best height %v, Hash %v, Got block %v\n", snapshot.Height, snapshot.Hash, block.Hash()) // 현재 체인의 최고 높이와 해시, 수신된 블록 해시 출력인데 blcok.Hash에서 체인팁 1차 나옴
 
 	isMainChain, _, err := chain.ProcessBlock(block, blockchain.BFNone)
-	if err != nil {
+	if err != nil { // 305를 받아야하는데 얘를 받아버림 / 블록 검증 실패
 		txs := block.Transactions()
 		fmt.Println(txs[0].Hash())
-		fmt.Printf("block validation failed for %s: %v\n", block.Hash().String(), err)
+		fmt.Printf("Block validation failed for %s: %v\n", block.Hash().String(), err)
 		return nil
 	}
-
 	if !isMainChain {
-		fmt.Printf("Received orphan block: %s, %v\n", block.Hash().String(), err)
-		return nil
+		fmt.Printf("Received orphan block: %s, %v\n", block.Hash().String(), err) //block.Hahs는 바꾸기 스냅샤스로
+		if len(blocksInQueue) == 0 {                                              // 대기 중인 블록이 없을 때 추가 getblocks 요청
+			blockLocator := blockchain.BlockLocator([]*chainhash.Hash{&snapshot.Hash}) // 현재 블록 해시를 사용하여 블록 로케이터 생성
+			getBlocksMsg := &wire.MsgGetBlocks{
+				ProtocolVersion:    wire.FeeFilterVersion,
+				BlockLocatorHashes: blockLocator,
+				HashStop:           *targetBlockHash,
+			}
+			err = wire.WriteMessage(conn, getBlocksMsg, wire.FeeFilterVersion, netParams.Net)
+			return nil
+		}
 	}
 
 	if targetBlockHash.IsEqual(block.Hash()) {
@@ -299,9 +303,9 @@ func handleBlockMessage(block *btcutil.Block, chain *blockchain.BlockChain, bloc
 		os.Exit(0)
 	}
 
-	fmt.Println("blocksinqueue", len(blocksInQueue)) // 현재 대기 중인 블록 수 출력
+	fmt.Println("Blocksinqueue", len(blocksInQueue)) // 현재 대기 중인 블록 수 출력 / 얘가 나와야하는데 286이 나옴
 	if len(blocksInQueue) == 1 {
-		for k, v := range blocksInQueue {
+		for k, v := range blocksInQueue { // 대기 중인 블록 해시 출력
 			fmt.Println(k, v)
 		}
 	}
@@ -314,7 +318,7 @@ func handleBlockMessage(block *btcutil.Block, chain *blockchain.BlockChain, bloc
 		}
 		err = wire.WriteMessage(conn, getBlocksMsg, wire.FeeFilterVersion, netParams.Net)
 		if err != nil {
-			return fmt.Errorf("failed to send next getblocks: %v", err)
+			return fmt.Errorf("ailed to send next getblocks: %v", err)
 		}
 		fmt.Println("Sent additional getblocks request")
 	}
